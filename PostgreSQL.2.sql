36a3592c64b4cc8ae247a34551d6fc1fe2facb04
Lu <132112201+LucianaMarques97@users.noreply.github.com>
2023-11-27 18:28:31 -0300

    Update PostgreSQL.2.sql

--- PostgreSQL.2.sql
+++ PostgreSQL.2.sql
@@ -1,1 +1,13 @@
- SELECT * FROM aluno;
+CREATE DATABASE ESCOLA;
+CREATE TABLE ALUNO (
+  aluno_id INT PRIMARY KEY,
+  nome VARCHAR(30),
+  email VARCHAR(100),
+  endereço VARCHAR(100)
+)
+ INSERT INTO aluno (aluno_id, nome, email, endereço)
+ VALUES (1, 'João Carlos', 'joao.carlos@email.com', 'Rua 13 de Maio'),
+ INSERT INTo aluno (aluno_id, nome, email, endereço)
+ VALUES (2, 'Luciana Marques', 'luciana.marques@email.com', 'Rua 06 de Julho');
+
+SELECT * FROM aluno;
